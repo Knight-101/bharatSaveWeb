54856c734a11aefbd2d3fe44b8d826dd80cb1095
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-08-16 20:58:44 +0530

    bug

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import AuthRoute from "./AuthRoute";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
+// const fs = require("fs");
+
+// const file = fs.readFileSync("../test");
 
 function App() {
   return (
@@ -14,6 +17,11 @@ function App() {
         </Route>
         <Route exact path="/">
           <Login />
+        </Route>
+        <Route path="/.well-known/acme-challenge/waX45KNreAxDZvdqVwbxytSwlSAX2lvloN05TDPDs0U">
+          <p>
+            waX45KNreAxDZvdqVwbxytSwlSAX2lvloN05TDPDs0U.que1Rpoum2rbib_LYPfEuYqYxPdm-tGHaTgtthWP_x8
+          </p>
         </Route>
         <AuthRoute path="/home" component={Home} />
         {/* <Route path="/register">
